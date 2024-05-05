e4c69dbd0c7923040fdb86ee4af2dd311052dc39
HawksDarris <darris.hawks@gmail.com>
2024-05-06 05:47:00 +0800

    $message

--- hosts/default/home.nix
+++ hosts/default/home.nix
@@ -1,21 +1,21 @@
 { config, pkgs, inputs, ... }:
 
 {
-  # Home Manager needs a bit of information about you and the paths it should
-  # manage.
- 
-    imports =
-      [
-	../../modules/home-manager/git.nix
-	../../modules/home-manager/hyprland.nix
-	../../modules/home-manager/kitty.nix
-	../../modules/home-manager/lf.nix
-	../../modules/home-manager/nushell.nix
-	../../modules/home-manager/waybar.nix
-        ../../modules/home-manager/browsers.nix
-        ../../modules/home-manager/fonts.nix
-	inputs.nixvim.homeManagerModules.nixvim
-      ];
+# Home Manager needs a bit of information about you and the paths it should
+# manage.
+
+  imports =
+    [
+    ../../modules/home-manager/git.nix
+      ../../modules/home-manager/hyprland.nix
+      ../../modules/home-manager/kitty.nix
+      ../../modules/home-manager/lf.nix
+      ../../modules/home-manager/nushell.nix
+      ../../modules/home-manager/waybar.nix
+      ../../modules/home-manager/browsers.nix
+      ../../modules/home-manager/fonts.nix
+      inputs.nixvim.homeManagerModules.nixvim
+    ];
 
   home.username = "sour";
   home.homeDirectory = "/home/sour";
@@ -24,14 +24,16 @@
     enable = true;
     defaultEditor = true;
     colorschemes.rose-pine.enable = true;
-    # clipboard.register = "unnamedplus";
-    # clipboard.providers.wl-copy.enable
 
+    globals.mapleader = " ";
 
     plugins = {
       lightline.enable = true;
       surround.enable = true;
+      comment.enable = true;
       treesitter.enable = true;
+      nix.enable = true;
+      typst-vim.enable = true;
     };
 
     plugins.lsp = {
@@ -39,49 +41,57 @@
       servers = {
 	tsserver.enable = true;
 	lua-ls = {
-          enable = true;
-          settings.telemetry.enable = false;
-        };
-        rust-analyzer = {
-          enable = true;
+	  enable = true;
+	  settings.telemetry.enable = false;
+	};
+	rust-analyzer = {
+	  enable = true;
 	  installRustc = true;
-          installCargo = true;
-        };
+	  installCargo = true;
+	};
       };
     };
+
+    extraPlugins = with pkgs.vimPlugins; [
+    {
+      plugin = vimwiki;
+      config = "lua require(\"Comment\").setup()";
+    }
+    ];
 
     opts = {
       clipboard = [ "unnamedplus" ];
       number = true;         # Show line numbers
-      relativenumber = true; # Show relative line numbers
+	relativenumber = true; # Show relative line numbers
 
-      shiftwidth = 2;        # Tab width should be 2
+	shiftwidth = 2;        # Tab width should be 2
     };
     extraConfigLua = ''
       -- Print a little welcome message when nvim is opened!
       print("Hello world!")
-    '';
+      '';
     extraConfigVim = ''
-    '';
+      '';
+
   };
 
-  # This value determines the Home Manager release that your configuration is
-  # compatible with. This helps avoid breakage when a new Home Manager release
-  # introduces backwards incompatible changes.
-  #
-  # You should not change this value, even if you update Home Manager. If you do
-  # want to update the value, then make sure to first check the Home Manager
-  # release notes.
+# This value determines the Home Manager release that your configuration is
+# compatible with. This helps avoid breakage when a new Home Manager release
+# introduces backwards incompatible changes.
+#
+# You should not change this value, even if you update Home Manager. If you do
+# want to update the value, then make sure to first check the Home Manager
+# release notes.
   home.stateVersion = "23.11"; # Please read the comment before changing.
 
-  # The home.packages option allows you to install Nix packages into your
-  # environment.
-  home.packages = with pkgs; [
-    # # Adds the 'hello' command to your environment. It prints a friendly
-    # # "Hello, world!" when run.
-    # pkgs.hello
-      # neovim
-      emacs 
+# The home.packages option allows you to install Nix packages into your
+# environment.
+    home.packages = with pkgs; [
+# # Adds the 'hello' command to your environment. It prints a friendly
+# # "Hello, world!" when run.
+# pkgs.hello
+# neovim
+    emacs 
       gh
       tldr
       fd
@@ -92,79 +102,79 @@
       rsync
       fzf
 
-	# sound
-  	pavucontrol
-  	pamixer
-  	brightnessctl
-	mpc-cli
-	ncmpcpp
-	playerctl
+# sound
+      pavucontrol
+      pamixer
+      brightnessctl
+      mpc-cli
+      ncmpcpp
+      playerctl
 
-	# To sort
-	# lxqt-policykit-agent
-	pandoc
-	dbus
-	pciutils
-	libnotify
-	ueberzug # for lf previews
-	jq
-	glib
-	bat
-	ncdu
-	dunst
-	mako # Maybe replace dunst
-	kitty
-	swww
-	wlogout
-	eww
-	brightnessctl
-	hyprpicker
-	rofi-wayland
-	rofimoji
-	slurp
-	swappy
-	swaylock
-	wf-recorder
-	wl-gammactl
-	wl-clipboard
-	wofi
-	wayshot
-	xdotool
+# To sort
+# lxqt-policykit-agent
+      pandoc
+      dbus
+      pciutils
+      libnotify
+      ueberzug # for lf previews
+      jq
+      glib
+      bat
+      ncdu
+      dunst
+      mako # Maybe replace dunst
+      kitty
+      swww
+      wlogout
+      eww
+      brightnessctl
+      hyprpicker
+      rofi-wayland
+      rofimoji
+      slurp
+      swappy
+      swaylock
+      wf-recorder
+      wl-gammactl
+      wl-clipboard
+      wofi
+      wayshot
+      xdotool
 
-    # # It is sometimes useful to fine-tune packages, for example, by applying
-    # # overrides. You can do that directly here, just don't forget the
-    # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
-    # # fonts?
-    # (pkgs.nerdfonts.override { fonts = [ "FantasqueSansMono" ]; })
+# # It is sometimes useful to fine-tune packages, for example, by applying
+# # overrides. You can do that directly here, just don't forget the
+# # parentheses. Maybe you want to install Nerd Fonts with a limited number of
+# # fonts?
+# (pkgs.nerdfonts.override { fonts = [ "FantasqueSansMono" ]; })
 
-    # # You can also create simple shell scripts directly inside your
-    # # configuration. For example, this adds a command 'my-hello' to your
-    # # environment:
-    # (pkgs.writeShellScriptBin "my-hello" ''
-    #   echo "Hello, ${config.home.username}!"
-    # '')
-  ];
+# # You can also create simple shell scripts directly inside your
+# # configuration. For example, this adds a command 'my-hello' to your
+# # environment:
+# (pkgs.writeShellScriptBin "my-hello" ''
+#   echo "Hello, ${config.home.username}!"
+# '')
+      ];
 
-  # Home Manager is pretty good at managing dotfiles. The primary way to manage
-  # plain files is through 'home.file'.
+# Home Manager is pretty good at managing dotfiles. The primary way to manage
+# plain files is through 'home.file'.
   home.file = {
-    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
-    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
-    # # symlink to the Nix store copy.
-    # ".screenrc".source = dotfiles/screenrc;
+# # Building this configuration will create a copy of 'dotfiles/screenrc' in
+# # the Nix store. Activating the configuration will then make '~/.screenrc' a
+# # symlink to the Nix store copy.
+# ".screenrc".source = dotfiles/screenrc;
 
-    # # You can also set the file content immediately.
-    # ".gradle/gradle.properties".text = ''
-    #   org.gradle.console=verbose
-    #   org.gradle.daemon.idletimeout=3600000
-    # '';
+# # You can also set the file content immediately.
+# ".gradle/gradle.properties".text = ''
+#   org.gradle.console=verbose
+#   org.gradle.daemon.idletimeout=3600000
+# '';
   };
 
   home.sessionVariables = {
-    # Didn't work for some reason. Will move to nushell.nix and see.
-    # EDITOR = "nvim";
+# Didn't work for some reason. Will move to nushell.nix and see.
+# EDITOR = "nvim";
   };
 
-  # Let Home Manager install and manage itself.
+# Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
 }
