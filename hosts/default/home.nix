4b68c3f8d542da4280a14a44d36e2f0dfa3b4e41
HawksDarris <darris.hawks@gmail.com>
2024-05-06 05:17:17 +0800

    $message

--- hosts/default/home.nix
+++ hosts/default/home.nix
@@ -23,6 +23,8 @@
   programs.nixvim = {
     enable = true;
     defaultEditor = true;
+    colorscheme.gruvbox.enable = true;
+
     plugins = {
       # lightline.enable = true;
       lualine.enable = true;
