d09bcac9d1ad311b57c21acbeac06cb9356f0679
HawksDarris <darris.hawks@gmail.com>
2024-05-06 07:21:19 +0800

    $message

--- hosts/default/home.nix
+++ hosts/default/home.nix
@@ -14,101 +14,11 @@
       ../../modules/home-manager/waybar.nix
       ../../modules/home-manager/browsers.nix
       ../../modules/home-manager/fonts.nix
-      inputs.nixvim.homeManagerModules.nixvim
+      ../../modules/home-manager/../nixvim.nix
     ];
 
   home.username = "sour";
   home.homeDirectory = "/home/sour";
-
-  programs.nixvim = {
-    enable = true;
-    defaultEditor = true;
-    colorschemes.catppuccin = {
-      enable = true;
-      settings = {
-        flavour = "latte";
-        integrations = {
-          cmp = true;
-          gitsigns = true;
-          mini = {
-            enabled = true;
-            indentscope_color = "";
-          };
-          treesitter = true;
-        };
-        styles = {
-          booleans = [
-            "bold"
-            "italic"
-          ];
-          conditionals = [
-            "bold"
-          ];
-        };
-        term_colors = true;
-      };
-    };
-
-    globals.mapleader = " ";
-
-    plugins = {
-      cmp.enable = true;
-      cmp-buffer.enable = true;
-      cmp-cmdline.enable = true;
-      cmp-nvim-lsp.enable = true;
-      cmp-path.enable = true;
-      cmp_luasnip.enable = true;
-      comment.enable = true;
-      friendly-snippets.enable = true;
-      fzf-lua.enable = true;
-      lightline.enable = true;
-      lint.enable = true; # TODO set up linting https://github.com/mfussenegger/nvim-lint
-      luasnip.enable = true;
-      nix.enable = true;
-      surround.enable = true;
-      telescope.enable = true;
-      treesitter.enable = true;
-      typst-vim.enable = true;
-    };
-
-    plugins.lsp = {
-      enable = true;
-      servers = {
-        tsserver.enable = true;
-        lua-ls = {
-          enable = true;
-          settings.telemetry.enable = false;
-        };
-        rust-analyzer = {
-          enable = true;
-          installRustc = true;
-          installCargo = true;
-        };
-      };
-    };
-
-    extraPlugins = with pkgs.vimPlugins; [
-      {
-        plugin = vimwiki;
-        config = "";
-      }
-    ];
-
-    opts = {
-      clipboard = [ "unnamedplus" ];
-      number = true;         # Show line numbers
-      relativenumber = true; # Show relative line numbers
-
-      shiftwidth = 2;        # Tab width should be 2
-    };
-    extraConfigLua = ''
-    -- Print a little welcome message when nvim is opened!
-    print("Hello world!")
-    '';
-    extraConfigVim = ''
-    '';
-
-  };
 
 # This value determines the Home Manager release that your configuration is
 # compatible with. This helps avoid breakage when a new Home Manager release
---
+++ modules/home-manager/nixvim/nixvim.nix
@@ -0,0 +1,97 @@
+{  
+  imports =
+    [
+      inputs.nixvim.homeManagerModules.nixvim
+    ];
+
+    programs.nixvim = {
+      enable = true;
+      defaultEditor = true;
+      colorschemes.catppuccin = {
+        enable = true;
+        settings = {
+          flavour = "latte";
+          integrations = {
+            cmp = true;
+            gitsigns = true;
+            mini = {
+              enabled = true;
+              indentscope_color = "";
+            };
+            treesitter = true;
+          };
+          styles = {
+            booleans = [
+              "bold"
+              "italic"
+            ];
+            conditionals = [
+              "bold"
+            ];
+          };
+          term_colors = true;
+        };
+      };
+
+      globals.mapleader = " ";
+
+      plugins = {
+        cmp.enable = true;
+        cmp-buffer.enable = true;
+        cmp-cmdline.enable = true;
+        cmp-nvim-lsp.enable = true;
+        cmp-path.enable = true;
+        cmp_luasnip.enable = true;
+        comment.enable = true;
+        friendly-snippets.enable = true;
+        fzf-lua.enable = true;
+        lightline.enable = true;
+        lint.enable = true; # TODO set up linting https://github.com/mfussenegger/nvim-lint
+        luasnip.enable = true;
+        nix.enable = true;
+        surround.enable = true;
+        telescope.enable = true;
+        treesitter.enable = true;
+        typst-vim.enable = true;
+      };
+
+      plugins.lsp = {
+        enable = true;
+        servers = {
+          tsserver.enable = true;
+          lua-ls = {
+            enable = true;
+            settings.telemetry.enable = false;
+          };
+          rust-analyzer = {
+            enable = true;
+            installRustc = true;
+            installCargo = true;
+          };
+        };
+      };
+
+      extraPlugins = with pkgs.vimPlugins; [
+      # {
+      #   plugin = vimwiki;
+      #   config = "";
+      # }
+    ];
+
+    opts = {
+      clipboard = [ "unnamedplus" ];
+      number = true;         # Show line numbers
+      relativenumber = true; # Show relative line numbers
+
+      shiftwidth = 2;        # Tab width should be 2
+    };
+    extraConfigLua = ''
+    -- Print a little welcome message when nvim is opened!
+    print("Hello world!")
+    '';
+    extraConfigVim = ''
+    '';
+
+  };
+
+}
