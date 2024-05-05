3221cf8aaac4d99fca995d6d14a718ba23ab2866
HawksDarris <darris.hawks@gmail.com>
2024-05-05 12:03:44 +0800

    $message

--- modules/home-manager/browsers.nix
+++ modules/home-manager/browsers.nix
@@ -7,38 +7,45 @@
 programs.firefox = {
   enable = true;
   profiles.sour = {
-    bookmarks = [
-      {
-        name = "kehua";
-	tags = [ "TEFL" "repo" "github" "git" ];
-	keyword = "kehua";
-	url = "https://github.com/HawksDarris/Kehua"
-      },
-      {
-        name = "toPhonetics";
-	tags = [ "TEFL" ];
-	keyword = "phonetics";
-	url = "https://tophonetics.com/";
-      }
-      {
-        name = "Grade 7 Lesson Plans";
-	tags = [ "TEFL" ];
-	keyword = "G7";
-	url = "file:///home/sour/share/Teaching/TEFL/Lesson%20Plans/Grade%207%20Lesson%20Plans.html";
-      }
-      {
-        name = "Grade 6 Lesson Plans";
-	tags = [ "TEFL" ];
-	keyword = "G6";
-	url = "file:///home/sour/share/Teaching/TEFL/Lesson%20Plans/Grade%206%20Lesson%20Plans.html";
-      }
-      {
-        name = "Home-Manager Options";
-	tags = [ "nix" "options" ];
-	keyword = "ho";
-	url = "https://home-manager-options.extranix.com/";
-      }
-    ];
+      bookmarks = [
+        {
+          name = "wikipedia";
+          tags = [ "wiki" ];
+          keyword = "wiki";
+          url = "https://en.wikipedia.org/wiki/Special:Search?search=%s&go=Go";
+        }
+    # bookmarks = [
+    #   {
+    #     name = "kehua";
+    #     tags = [ "TEFL" "repo" "github" "git" ];
+    #     keyword = "kehua";
+    #     url = "https://github.com/HawksDarris/Kehua"
+    #   }
+    #   {
+    #     name = "toPhonetics";
+    #     tags = [ "TEFL" ];
+    #     keyword = "phonetics";
+    #     url = "https://tophonetics.com/";
+    #   }
+    #   {
+    #     name = "Grade 7 Lesson Plans";
+    #     tags = [ "TEFL" ];
+    #     keyword = "G7";
+    #     url = "file:///home/sour/share/Teaching/TEFL/Lesson%20Plans/Grade%207%20Lesson%20Plans.html";
+    #   }
+    #   {
+    #     name = "Grade 6 Lesson Plans";
+    #     tags = [ "TEFL" ];
+    #     keyword = "G6";
+    #     url = "file:///home/sour/share/Teaching/TEFL/Lesson%20Plans/Grade%206%20Lesson%20Plans.html";
+    #   }
+    #   {
+    #     name = "Home-Manager Options";
+    #     tags = [ "nix" "options" ];
+    #     keyword = "ho";
+    #     url = "https://home-manager-options.extranix.com/";
+    #   }
+    # ];
 
     settings = {
       "dom.security.https_only_mode" = true;
