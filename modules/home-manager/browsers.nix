52c6ab62c69f299c10d32596306ca1725c9367bc
HawksDarris <darris.hawks@gmail.com>
2024-05-05 12:05:06 +0800

    $message

--- modules/home-manager/browsers.nix
+++ modules/home-manager/browsers.nix
@@ -14,30 +14,6 @@ programs.firefox = {
         keyword = "kehua";
         url = "https://github.com/HawksDarris/Kehua"
       }
-      {
-        name = "toPhonetics";
-        tags = [ "TEFL" ];
-        keyword = "phonetics";
-        url = "https://tophonetics.com/";
-      }
-      {
-        name = "Grade 7 Lesson Plans";
-        tags = [ "TEFL" ];
-        keyword = "G7";
-        url = "file:///home/sour/share/Teaching/TEFL/Lesson%20Plans/Grade%207%20Lesson%20Plans.html";
-      }
-      {
-        name = "Grade 6 Lesson Plans";
-        tags = [ "TEFL" ];
-        keyword = "G6";
-        url = "file:///home/sour/share/Teaching/TEFL/Lesson%20Plans/Grade%206%20Lesson%20Plans.html";
-      }
-      {
-        name = "Home-Manager Options";
-        tags = [ "nix" "options" ];
-        keyword = "ho";
-        url = "https://home-manager-options.extranix.com/";
-      }
     ];
 
     settings = {
