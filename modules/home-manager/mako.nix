f366d47e57aae1be83502c0789bfdc12eed5e91f
HawksDarris <darris.hawks@gmail.com>
2024-05-10 08:17:10 +0800

    customize mako

--- modules/home-manager/mako.nix
+++ modules/home-manager/mako.nix
@@ -5,8 +5,13 @@
     backgroundColor = "#${base01}";
     borderColor = "#${base0E}";
     textColor = "#${base04}";
+    width = 300; 
+    height = 300;
     borderRadius = 5;
     borderSize = 2;
+    maxVisible = 20;
+    font = "JetBrainsMono Nerd Font 8";
     layer = "overlay";
+    defaultTimeout = 5000;
   };
 }
