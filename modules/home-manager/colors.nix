f92b34bc2ca36f580c873f3acae1a4b79a7ca011
HawksDarris <darris.hawks@gmail.com>
2024-05-16 07:26:40 +0800

    play with colors

--- modules/home-manager/colors.nix
+++ modules/home-manager/colors.nix
@@ -7,33 +7,33 @@ let
   # catppuccin-latte;
   # dracula
 in {
-  colorScheme = inputs.nix-colors.colorSchemes.${theme};
+  # colorScheme = inputs.nix-colors.colorSchemes.${theme};
   # can override with pywal
 
   # TODO Modularize
-  # colorScheme = {
-  #   slug = "pywalgen";
-  #   name = "Pywal Generated";
-  #   author = "A computer";
-  #   palette = {
-  #     base00 = "#100614";
-  #     base01 = "#4F4EB4";
-  #     base02 = "#DC61A6";
-  #     base03 = "#4D9BDD";
-  #     base04 = "#F4A3A9";
-  #     base05 = "#F0CBB2";
-  #     base06 = "#A6A2DE";
-  #     base07 = "#edcde4";
-  #     base08 = "#a58f9f";
-  #     base09 = "#4F4EB4";
-  #     base0A = "#DC61A6";
-  #     base0B = "#4D9BDD";
-  #     base0C = "#F4A3A9";
-  #     base0D = "#F0CBB2";
-  #     base0E = "#A6A2DE";
-  #     base0F = "#59325C";
-  #   };
-  # };
+  colorScheme = {
+    slug = "pywalgen";
+    name = "Pywal Generated";
+    author = "A computer";
+    palette = {
+      base00 = "#100614";
+      base01 = "#4F4EB4";
+      base02 = "#DC61A6";
+      base03 = "#4D9BDD";
+      base04 = "#F4A3A9";
+      base05 = "#F0CBB2";
+      base06 = "#A6A2DE";
+      base07 = "#edcde4";
+      base08 = "#a58f9f";
+      base09 = "#4F4EB4";
+      base0A = "#DC61A6";
+      base0B = "#4D9BDD";
+      base0C = "#F4A3A9";
+      base0D = "#F0CBB2";
+      base0E = "#A6A2DE";
+      base0F = "#59325C";
+    };
+  };
 
   gtk = {
     enable = true;
@@ -60,6 +60,25 @@ in {
     };
   };
 
+
+  programs.pywal.enable = true;
+
+# Reference example for specialization
+
+  #   home.packages = with pkgs; [
+  #     (hiPrio (writeShellApplication {
+  #       name = "toggle-theme";
+  #       runtimeInputs = with pkgs; [ home-manager coreutils ripgrep ];
+  #         # go back two generations
+  #         # home-manager creates a new generation every time activation script invoked
+  #         text = ''
+  #           "$(home-manager generations | head -2 | tail -1 | rg -o '/[^ ]*')"/activate
+  #           '';
+  #       }
+  #     )
+  #   )
+  # ];
+
   #   specialisation.light-theme.configuration = {
   #   colorScheme = lib.mkForce { 
   #     slug =  "catppuccinLatte";
@@ -83,20 +102,4 @@ in {
   #     };
   #   };
   # };
-
-  programs.pywal.enable = true;
-
-  home.packages = with pkgs; [
-    (hiPrio (writeShellApplication {
-      name = "toggle-theme";
-      runtimeInputs = with pkgs; [ home-manager coreutils ripgrep ];
-        # go back two generations
-        # home-manager creates a new generation every time activation script invoked
-        text = ''
-          "$(home-manager generations | head -2 | tail -1 | rg -o '/[^ ]*')"/activate
-          '';
-      }
-    )
-  )
-];
 }
