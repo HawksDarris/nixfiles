20338c5e18aad16394e1d63328fdd8ec1111fc26
HawksDarris <darris.hawks@gmail.com>
2024-05-15 14:22:56 +0800

    add git.nix

---
+++ modules/home-manager/git.nix
@@ -0,0 +1,19 @@
+{
+  programs.git = {
+    enable = true;
+    userName = "HawksDarris";
+    userEmail = "darris.hawks@gmail.com";
+    diff-so-fancy = {
+      enable = true;
+      changeHunkIndicators = true;
+    };
+    extraConfig =
+      {
+        url = {
+          "https://mirror.ghproxy.com/https://github" = {
+            insteadOf = "https://github";
+          };
+        };
+      };
+  };
+}
