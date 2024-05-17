ca943fcb8949f5abecf0942d8a87773eda9f17d9
HawksDarris <darris.hawks@gmail.com>
2024-05-17 13:17:37 +0800

    fix typo emacs.nix

--- modules/home-manager/emacs.nix
+++ modules/home-manager/emacs.nix
@@ -1,23 +1,23 @@
-{ ... }:
+{ epkgs, ... }:
 
 {
   programs.emacs = {
     enable = true;
     extraPackages = epkgs: [
-      evil
-      evil-collection
-      evil-commentary
-      evil-surround
-      evil-numbers
-      company
-      company-org-block
-      company-nixos-options
-      corfu
-      flycheck
-      flycheck-popup-tip
-      org-re-reveal
-      nix-mode
-      nix-buffer
-    ]
+      epkgs.evil
+      epkgs.evil-collection
+      epkgs.evil-commentary
+      epkgs.evil-surround
+      epkgs.evil-numbers
+      epkgs.company
+      epkgs.company-org-block
+      epkgs.company-nixos-options
+      epkgs.corfu
+      epkgs.flycheck
+      epkgs.flycheck-popup-tip
+      epkgs.org-re-reveal
+      epkgs.nix-mode
+      epkgs.nix-buffer
+    ];
   };
 }
