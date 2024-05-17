64e29f38bcaba2de15e091acba8a702fb2d3f3a2
HawksDarris <darris.hawks@gmail.com>
2024-05-17 17:58:00 +0800

    refactor emacs.nix

--- modules/home-manager/emacs.nix
+++ modules/home-manager/emacs.nix
@@ -3,6 +3,9 @@
 # To view all emacs nix packages, 
 # nix-env -f '<nixpkgs>' -qaP -A emacsPackages
 {
+  imports = [
+#    ./emacs-extraConfig.nix
+  ];
   programs.emacs = {
     enable = true;
     extraPackages = epkgs: [
@@ -12,16 +15,25 @@
       epkgs.evil-surround
       epkgs.evil-numbers
       epkgs.company
-      epkgs.company-org-block
       epkgs.company-nixos-options
+      epkgs.company-org-block
+      epkgs.company-emojify
+      epkgs.company-shell
+      epkgs.company-spell
+      epkgs.company-suggest
+      epkgs.company-web
       epkgs.corfu
       epkgs.flycheck
       epkgs.flycheck-popup-tip
-      epkgs.org-re-reveal
-      epkgs.nix-mode
-      epkgs.nix-buffer
       epkgs.gruvbox-theme
+      epkgs.nix-buffer
+      epkgs.nix-mode
+      epkgs.org-auto-tangle
       epkgs.org-modern
+      epkgs.org-re-reveal
     ];
   };
+  programs.emacs.extraConfig = ''
+    (setq org-auto-tangle-default t)
+  '';
 }
