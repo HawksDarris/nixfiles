0c1a638e63f6d204165662f3907109b4d0fcd86a
HawksDarris <darris.hawks@gmail.com>
2024-05-29 15:06:22 +0800

    Change syncthing service

--- modules/home-manager/services.nix
+++ modules/home-manager/services.nix
@@ -8,7 +8,11 @@ services.emacs.client.enable = true;
 # Emacs:1 ends here
 
 # [[file:../../README.org::*Syncthing][Syncthing:1]]
-services.syncthing.enable = true;
+services.syncthing = {
+  enable = true;
+  # extraOptions = [
+  # ];
+};
 # Syncthing:1 ends here
 
 # [[file:../../README.org::*Closing][Closing:1]]
