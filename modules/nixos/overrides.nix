b0f10a836381aea592d4c0fb47c1881eccd1f8f9
HawksDarris <darris.hawks@gmail.com>
2024-06-05 09:02:42 +0800

    correct overrides.nix

--- modules/nixos/overrides.nix
+++ modules/nixos/overrides.nix
@@ -1,5 +1,5 @@
 {
-  packageOverrides = pkgs: {
+  nixpkgs.config.packageOverrides = pkgs: {
     nur = import (builtins.fetchTarball "https://github.com/nix-community/NUR/archive/master.tar.gz") {
       inherit pkgs;
     };
