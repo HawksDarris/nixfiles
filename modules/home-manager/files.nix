97dd16bf9bd0da3d3628210890e345ede98c4c5f
HawksDarris <darris.hawks@gmail.com>
2024-08-26 06:50:27 +0800

    add files created by home-manager
    temporarily add espanso configs in that directory
    because the espanso module isn't doing it for some reason

---
+++ modules/home-manager/files.nix
@@ -0,0 +1,10 @@
+# [[file:../../README.org::*Files I want created by home-manager][Files I want created by home-manager:1]]
+home.file = { ".config/espanso/configs/default.yml" =
+  { text =
+  ''
+
+  '';
+    executable = false;
+  };
+            };
+# Files I want created by home-manager:1 ends here
