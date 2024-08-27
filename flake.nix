f0f86b8150864634810416f28884d4aeec53e613
HawksDarris <darris.hawks@gmail.com>
2024-08-28 05:09:37 +0800

    remove erroneous line for nixpkgs

--- flake.nix
+++ flake.nix
@@ -12,9 +12,7 @@
   };
 
   inputs = {
-    # nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    nixpkgs.url = "https://mirrors.tuna.tsinghua.edu.cn/git/nixpkgs.git/";
-
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
     sops-nix.url = "github:Mic92/sops-nix";
 
