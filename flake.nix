1a70f600f787a4483083288bc10c42989283ca42
HawksDarris <darris.hawks@gmail.com>
2024-08-28 04:54:14 +0800

    update flake

--- flake.nix
+++ flake.nix
@@ -12,7 +12,9 @@
   };
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    # nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "https://mirrors.tuna.tsinghua.edu.cn/git/nixpkgs.git/";
+
 
     sops-nix.url = "github:Mic92/sops-nix";
 
