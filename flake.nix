aed6edbf7bae6c71f8a8e8db4cfb2dc537d18c7a
HawksDarris <darris.hawks@gmail.com>
2024-08-27 21:37:39 +0800

    add tsinghua substituter line

--- flake.nix
+++ flake.nix
@@ -1,6 +1,15 @@
 {
   description = "Vinegar Flakes";
 
+  nixConfig = {
+    extra-substituters = [
+      "https://mirrors.tuna.tsinghua.edu.cn/nix-channels/store"
+    ];
+
+    extra-trusted-public-keys = [
+    ];
+  };
+
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
