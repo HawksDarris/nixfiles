96e716ede50e6cc57d26ebb1a688db7cdd97e375
HawksDarris <darris.hawks@gmail.com>
2024-09-01 17:17:40 +0800

    remove extra-trusted-public-keys bit

--- flake.nix
+++ flake.nix
@@ -6,9 +6,9 @@
       "https://mirrors.tuna.tsinghua.edu.cn/nix-channels/store"
     ];
 
-    extra-trusted-public-keys = [
-      "AAAAC3NzaC1lZDI1NTE5AAAAIElKUkue9e/+1JjHvJocbCuF+71UmWfNj+TgOQ8kIGod"
-    ];
+    # extra-trusted-public-keys = [
+    #   ""
+    # ];
   };
 
   inputs = {
