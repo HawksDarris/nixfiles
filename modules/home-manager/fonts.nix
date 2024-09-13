836a12ce3f18d7fd85d57c5bd35cb0781746bdec
HawksDarris <darris.hawks@gmail.com>
2024-09-13 13:47:18 +0800

    add fonts; add font replacement rule

--- modules/home-manager/fonts.nix
+++ modules/home-manager/fonts.nix
@@ -1,15 +1,26 @@
 { pkgs, ... }:
 {
-  fonts.fontconfig.enable = true;
-    home.packages = with pkgs; [
-        fira-code
-        fira-code-symbols
-        font-awesome
-        liberation_ttf
-        mplus-outline-fonts.githubRelease
-        nerdfonts
-        noto-fonts
-        noto-fonts-emoji
-        proggyfonts
-];
+  fonts.fontconfig = {
+    enable = true;
+    localConf = ''
+      <match target="pattern">
+        <test qual="any" name="family"><string>NewCenturySchlbk</string></test>
+        <edit name="family" mode="assign" binding="same"><string>TeX Gyre Schola</string></edit>
+      </match>
+    '';
+  };
+  home.packages = with pkgs; [
+    fira-code
+    fira-code-symbols
+    font-awesome
+    liberation_ttf
+    mplus-outline-fonts.githubRelease
+    nerdfonts
+    noto-fonts
+    noto-fonts-emoji
+    proggyfonts
+    ubuntu_font_family
+    liberation_ttf
+    gyre-fonts
+  ];
 }
