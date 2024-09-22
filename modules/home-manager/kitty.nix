9e389b9608ec247d928b7fbdd4ce5a1871aff599
HawksDarris <darris.hawks@gmail.com>
2024-09-22 19:35:44 +0800

    change kitty font to firacode

--- modules/home-manager/kitty.nix
+++ modules/home-manager/kitty.nix
@@ -3,8 +3,8 @@
   programs.kitty = {
     enable = true;
     font = {
-      name = "JetBrainsMono Nerd Font";
-      size = 10;
+      name = "FiraCode Nerd Font Mono";
+      size = 12;
     };
     # theme = "Catppuccin-Mocha";
     settings = {
