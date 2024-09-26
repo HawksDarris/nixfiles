9728ef8bf6f21e355e9237beb917ae2991e5a12b
HawksDarris <darris.hawks@gmail.com>
2024-09-27 06:16:24 +0800

    modularize keymaps; move config into vanilla-config.nix

--- modules/home-manager/nixvim/default.nix
+++ modules/home-manager/nixvim/default.nix
@@ -8,6 +8,7 @@
       ./noice.nix
       ./bufferline.nix
       ./vanilla-config.nix
+      ./keymaps.nix
     ];
 
   programs.nixvim = {
@@ -37,7 +38,6 @@
         base0F = "#${base0F}";
       };
     };
-    globals.mapleader = " ";
     plugins = {
       cmp = {
         enable = true;
@@ -168,115 +168,7 @@
         ];
       };
     };
-    keymaps = [
-      {
-        mode = "n";
-        key = "<Tab>";
-        action = "<cmd>BufferLineCycleNext<cr>";
-        options = {
-          desc = "Cycle to next buffer";
-        };
-      }
 
-      {
-        mode = "n";
-        key = "<S-Tab>";
-        action = "<cmd>BufferLineCyclePrev<cr>";
-        options = {
-          desc = "Cycle to previous buffer";
-        };
-      }
-
-      {
-        mode = "n";
-        key = "<S-l>";
-        action = "<cmd>BufferLineCycleNext<cr>";
-        options = {
-          desc = "Cycle to next buffer";
-        };
-      }
-
-      {
-        mode = "n";
-        key = "<S-h>";
-        action = "<cmd>BufferLineCyclePrev<cr>";
-        options = {
-          desc = "Cycle to previous buffer";
-        };
-      }
-
-      {
-        mode = "n";
-        key = "<leader>bd";
-        action = "<cmd>bdelete<cr>";
-        options = {
-          desc = "Delete buffer";
-        };
-      }
-
-      {
-        mode = "n";
-        key = "<leader>bb";
-        action = "<cmd>e #<cr>";
-        options = {
-          desc = "Switch to Other Buffer";
-        };
-      }
-
-      # {
-      #   mode = "n";
-      #   key = "<leader>`";
-      #   action = "<cmd>e #<cr>";
-      #   options = {
-      #     desc = "Switch to Other Buffer";
-      #   };
-      # }
-
-      {
-        mode = "n";
-        key = "<leader>br";
-        action = "<cmd>BufferLineCloseRight<cr>";
-        options = {
-          desc = "Delete buffers to the right";
-        };
-      }
-
-      {
-        mode = "n";
-        key = "<leader>bl";
-        action = "<cmd>BufferLineCloseLeft<cr>";
-        options = {
-          desc = "Delete buffers to the left";
-        };
-      }
-
-      {
-        mode = "n";
-        key = "<leader>bo";
-        action = "<cmd>BufferLineCloseOthers<cr>";
-        options = {
-          desc = "Delete other buffers";
-        };
-      }
-
-      {
-        mode = "n";
-        key = "<leader>bp";
-        action = "<cmd>BufferLineTogglePin<cr>";
-        options = {
-          desc = "Toggle pin";
-        };
-      }
-
-      {
-        mode = "n";
-        key = "<leader>bP";
-        action = "<Cmd>BufferLineGroupClose ungrouped<CR>";
-        options = {
-          desc = "Delete non-pinned buffers";
-        };
-      }
-    ];
 
     plugins.lsp = {
       enable = true;
